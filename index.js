7b4b5be77085b4c2ce5c1e1ecee7610f015a2ee9
Joe Young <musicjoeyoung@gmail.com>
2021-05-20 16:41:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("Welcome to The Problem certificate maker.");
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+submitBtn.addEventListener("click", () => {
+  const val = userName.value;
+  if (val.trim() !== "" && userName.checkValidity()) {
+    // console.log(val);
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch("Certificate.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  //get font
+  const fontBytes = await fetch("Sanchez-Regular.ttf").then((res) =>
+    res.arrayBuffer()
+  );
+  // Embed our custom font in the document
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Draw a string of text diagonally across the first page
+  // Specify where the inputted text will land
+  firstPage.drawText(name, {
+    x: 300,
+    y: 270,
+    size: 58,
+    font: SanChezFont,
+    color: rgb(0, 0, 0),
+  });
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  saveAs(pdfDataUri, `${name}-is-Part-of-The-Problem__Membership.pdf`);
+};
