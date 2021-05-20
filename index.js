5beb8e328973bf598a7535c3a6b811904c55cd4d
Joe Young <musicjoeyoung@gmail.com>
2021-05-20 17:09:46 -0400

    temporary centering fix

--- index.js
+++ index.js
@@ -33,13 +33,65 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   // Specify where the inputted text will land
-  firstPage.drawText(name, {
-    x: 300,
-    y: 270,
-    size: 58,
-    font: SanChezFont,
-    color: rgb(0, 0, 0),
-  });
+
+  //This is a temporary pseudo-fix that will make the text render mostly centered.
+  if (name.length > 9 && name.length <= 15) {
+    firstPage.drawText(name, {
+      x: 200,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else if (name.length < 7 && name.length > 5) {
+    firstPage.drawText(name, {
+      x: 300,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else if (name.length > 14 && name.length < 22) {
+    firstPage.drawText(name, {
+      x: 150,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else if (name.length <= 3) {
+    firstPage.drawText(name, {
+      x: 340,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else if (name.length === 4) {
+    firstPage.drawText(name, {
+      x: 325,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else if (name.length >= 22) {
+    firstPage.drawText(name, {
+      x: 80,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  } else {
+    firstPage.drawText(name, {
+      x: 300,
+      y: 270,
+      size: 58,
+      font: SanChezFont,
+      color: rgb(0, 0, 0),
+    });
+  }
 
   // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
